cfd0b1b1aaf2ddac39f9c943f43e2638be9eb102
stlireri <stellaireri@gmail.com>
2018-09-21 15:06:34 +0300

    db config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', ''),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'database' => env('DB_DATABASE', 'onelove_app'),
+            'username' => env('DB_USERNAME', 'stella'),
+            'password' => env('DB_PASSWORD', 'stegas94'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
